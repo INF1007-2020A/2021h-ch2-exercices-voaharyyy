d6109f85110307166e671f001aa841f4acd48c26
Voahary Estelle Rajerison <voahary-estelle.rajerison@polymtl.ca>
2021-01-26 11:07:44 -0500

    la fonction est codée

--- exercice.py
+++ exercice.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
+    for k in range(len(mot)) :
+        lettre = mot[k]
+        positionMin = ord(lettre)
+        if positionMin >= 97 :
+            positionMaj = positionMin-32
+            lettre = chr(positionMaj)
+            mot = mot.replace(mot[k], lettre)
     return mot
 
 
